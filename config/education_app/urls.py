bf9af761a5c78f0ea0ad197bde86328ebd803806
Inna <MenelN@yandex.ru>
2026-01-04 06:32:32 +0300

    add admin, serializers.py, urls, views

---
+++ config/education_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CourseViewSet, LessonList, LessonDetail
+
+# Создание маршрутизатора для ViewSet
+router = DefaultRouter()
+router.register(r'courses', CourseViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),  # Все маршруты для курсов
+    path('lessons/', LessonList.as_view(), name='lesson-list'),  # Список и создание
+    path('lessons/<int:pk>/', LessonDetail.as_view(), name='lesson-detail'),  # Получение, изменение и удаление
+]
--- config/education_app/views.py
+++ config/education_app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import generics
+from .models import Course, Lesson
+from .serializers import CourseSerializer, LessonSerializer
 
-# Create your views here.
+# ViewSet для Курсов
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+# Generic-классы для Уроков
+class LessonList(generics.ListCreateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+class LessonDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
