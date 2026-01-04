70fff1e78c2aee7bc69273f5dbca08e4befc21c3
Inna <MenelN@yandex.ru>
2026-01-04 06:20:22 +0300

    add models and apps

--- config/config/settings.py
+++ config/config/settings.py
@@ -22,7 +22,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    "users",
+    "education_app",
 ]
 
 MIDDLEWARE = [
