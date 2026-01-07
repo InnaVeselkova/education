3ba343f2c9b31cee54c094dfc6365ed3856c0680
Inna <MenelN@yandex.ru>
2026-01-08 00:31:24 +0300

    add corrections  for CourseSerializer and add class Payment

--- education/education_app/serializers.py
+++ education/education_app/serializers.py
@@ -3,12 +3,21 @@ from .models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lesson_count = serializers.SerializerMethodField()
     class Meta:
         model = Course
         fields = "__all__"
+
+    def get_lesson_count(self, obj):
+        return obj.lessons.count()  # Возвращаем количество уроков, связанных с курсом
 
 
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = "__all__"
+
+
+
+
+
--- education/users/models.py
+++ education/users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from education.education_app.models import Course, Lesson
+
 
 class User(AbstractUser):
     username = None
@@ -32,3 +34,27 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class Payment(models.Model):
+    PAYMENT_METHOD_CHOICES = [
+        ('cash', 'Наличные'),
+        ('transfer', 'Перевод на счет'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Ссылка на пользователя
+    payment_date = models.DateTimeField(auto_now_add=True)  # Дата и время оплаты
+    course = models.ForeignKey(Course, null=True, blank=True,
+                               on_delete=models.CASCADE)  # Ссылка на оплаченный курс (может быть пустым)
+    lesson = models.ForeignKey(Lesson, null=True, blank=True,
+                               on_delete=models.CASCADE)  # Ссылка на оплаченный урок (может быть пустым)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)  # Сумма оплаты
+    payment_method = models.CharField(max_length=10, choices=PAYMENT_METHOD_CHOICES)  # Способ оплаты
+
+    def __str__(self):
+        payment_method_display = self.get_payment_method_display() if self.payment_method else 'Неизвестный способ'
+        return f"Платеж от {self.user.username} - сумма: {self.amount} ({payment_method_display})"
+
+    class Meta:
+        verbose_name = "Платеж"
+        verbose_name_plural = "Платежи"
